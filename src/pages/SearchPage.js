4615f82ae77f679d37952f564019935158f127cc
dboren <danielboren@att.net>
2021-05-28 22:15:20 -0500

    set initial state of Search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect}  from "react";
 
 import Navbar from "../components/Navbar";
 
 function SearchPage() {
 
+    const [SearchPage, setSearchPage] = useState([])
+    const [formObject, setFormObject] = useState({})
 
 
 };
