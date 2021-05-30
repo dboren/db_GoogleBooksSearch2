aa27b0ca55d8936c995d08c899652de38c9c2e06
dboren <danielboren@att.net>
2021-05-29 22:08:41 -0500

    added API search function to API util

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const API_KEY = process.env.API_KEY;
 
 export default {
     getBooks: function() {
@@ -12,5 +13,8 @@ export default {
     },
     saveBook: function(bookData) {
         return axios.post("/api/books", bookData);
+    },
+    findBook: function() {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=search-terms&key=${API_KEY}`)
     }
 };
